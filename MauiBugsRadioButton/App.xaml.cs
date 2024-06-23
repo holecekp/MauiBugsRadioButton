de402907de163d80db594b384986524494ddc2c0
holecekp <holecekp@holecekp.com>
2024-06-23 10:33:05 +0200

    Add project files.

---
+++ MauiBugsRadioButton/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiBugsRadioButton
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiBugsRadioButton/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiBugsRadioButton
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
