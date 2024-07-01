e97fd2dedbe756957e777600814ac4ac41b609e8
holecekp <holecekp@holecekp.com>
2024-07-01 09:18:19 +0200

    Int.Parse test - it does not work correctly on x86_64 as it can be seen in the logcat - it adds a bunch of invisible characters at the beginning

--- MauiBugsRadioButton/MainPage.xaml.cs
+++ MauiBugsRadioButton/MainPage.xaml.cs
@@ -5,6 +5,32 @@
         public MainPage()
         {
             InitializeComponent();
+
+            string two = 2.ToString();
+            LabelTwo.Text = two;
+            LabelTwoIsCorrect.Text = IsCorrectText(two, "2");
+
+            string twelve = 12.ToString();
+            LabelTwelve.Text = twelve;
+            LabelTwelveIsCorrect.Text = IsCorrectText(twelve, "12");
+
+            string twoHundred = 200.ToString();
+            LabelTwohundred.Text = twoHundred;
+            LabelTwohundredIsCorrect.Text = IsCorrectText(twoHundred, "200");
+        }
+
+        private string IsCorrectText(string actual, string expected)
+        {
+            AndroidLog($"Testing Int.Parse - Expected: {expected}. Actual: {actual}");
+
+            return actual == expected ? "OK" : "Incorrect !!!!!";
+        }
+
+        public static void AndroidLog(string s)
+        {
+#if ANDROID
+            Android.Util.Log.Info("Int.ParseTest", s);
+#endif
         }
     }
 
